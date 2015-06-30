e2be9f803e39ad51bede45c2f68100711b51784b
Keith Boehler - SS15 <kboehler@polaris.cv.nrao.edu>
2015-06-30 12:02:18 -0400

    Adding the abbility to read items from a text file into a array

--- fileWorker.php
+++ fileWorker.php
@@ -3,12 +3,14 @@
 require_once(dirname(__FILE__) . '/../SiteConfig.php'); // def sig get configmain
 
 
-class fileWorker{
+class fileWorker
+{
 	
 	private $dataName;
 	private $dataAdress;
 	
-	public function __construct(){
+	public function __construct()
+	{
 		
 		require(site_get_config_main());
 		// establish defult directory for storage of files
@@ -17,23 +19,37 @@ class fileWorker{
 		{
 			mkdir($masterDir);
 		}
-		
 		$this->dataName = "data.txt";
 		$this->dataAdress = $masterDir;
 		
 	}// end of constructor
 	
-	private function handleMaker(){
+	private function toArray()
+	{
+		$rawHandel = $this->dataAdress . $this-> dataName;
+		$rawArray = array();
+		$row = array();
+		if (file_exists($rawHandel))
+		{
+			// reading in the lines 
+			$f = fopen($rawHandel, 'r');
+			while (($row = fgets($f)) !== false)
+			{
+				$rawArray[] = explode(";", $row);
+			}// end of while loop
+			fclose($f);			
+		}
+		else 
+			die("ERROR: file does not exist ->" . $rawHandel);
+		return $rawArray;
 		
-		return $rawHandel = $this->dataAdress . $this-> dataName ;
-		
-	}// end of handle maker
+	}// end of toArray 	
 
 	
 	
 	
 	
-}// endo f class 
+}// endo fileWorker class 
 
 
 ?>
