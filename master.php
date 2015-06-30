b0f5cff7279dcb3c3626aadebb98baa494b6b014
Keith Boehler <germanbohler@gmail.com>
2015-06-30 16:49:07 -0400

    established link between master and fileworker to bring forth array generated from to array function in fileWorker

--- master.php
+++ master.php
@@ -1,16 +1,19 @@
 <?php
+echo "Start <br>";
 /*
  *  This is the master php file that will be calling on the classes 
  *  deffer all all working code to other classes 
  */
-
+// needed classes 
+require 'arrayWorker.php';
 require 'fileWorker.php';
 
-$test = new fileWorker();
-
-echo $test->handleMaker();
-
-
+// generate array from text file 
+$rawArrayObj = new fileWorker();
+$rawArray = $rawArrayObj-> toArray();
 
 
+//select data to be ploted
+
+echo "End";
 ?>
