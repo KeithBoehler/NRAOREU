226eb27d0740f46d78c620c9c972e9ded4c3d55c
Keith Boehler <germanbohler@gmail.com>
2015-07-01 11:15:58 -0400

    made calling for to arrayWorker Class to retreave the data that will be later written to the new .txt file

--- master.php
+++ master.php
@@ -2,18 +2,23 @@
 echo "Start <br>";
 /*
  *  This is the master php file that will be calling on the classes 
- *  deffer all all working code to other classes 
+ *  deffer all all working code to other classes \
+ *  /home/webtest.cv.nrao.edu/active/php/ntc/ws-kb
  */
 // needed classes 
 require 'arrayWorker.php';
 require 'fileWorker.php';
 
 // generate array from text file 
-$rawArrayObj = new fileWorker();
-$rawArray = $rawArrayObj-> toArray();
-
-
+$fileWorkerObj = new fileWorker();
+$rawArray = $fileWorkerObj-> toArray();
 //select data to be ploted
+$arrayWorkerObj = new arrayWorker($rawArray);
+$VLA = $arrayWorkerObj ->arrayMerger($rawArray, $AllanVar, $Time);
+//Write refined data poins to .txt file so that GNUPlot may use
+
+
+
 
 echo "End";
 ?>
