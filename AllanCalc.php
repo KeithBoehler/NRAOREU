beca34376acc67b48bd9a58d85677b5c58f278df
Keith Boehler <germanbohler@gmail.com>
2015-07-24 13:28:26 -0400

    changed the allan variace calulating function in to private to be called by the constructor. This is so that the constructor may have a loop to call the function multiple times and thus generate the needed points

--- AllanCalc.php
+++ AllanCalc.php
@@ -8,8 +8,12 @@ class allanCalc {
 	private $meanOfDataSet;
 	private $tou0;
 	
-	public function __construct() {
-		
+	public function __construct($data) {
+		$allanArray = array();
+		for ($i = 0; $i < count($data); ++$i) {
+			$allanArray[$i] = $this->allanVariance($data);
+		}
+		return $allanArray;
 	}// end of constructor 
 	/**
 	 * 
@@ -17,14 +21,18 @@ class allanCalc {
 	 * @return number: allan variace 
 	 * see FEND-40.00.00.00-079-A35-PRO Page: Page 115 of 183 for more detail. 
 	 */
-	public function allanVariance($rawData) {
-		$this->numberOfSamples = count($rawData);
+	private function allanVariance($rawData) {
 		$mu = $this->average($rawData);
-		$coeff = 1 / (2 * ($this->numberOfSamples - 1) * pow($mu, 2));
-		$sum = $this->summation($rawData);
-		$b = $this->dataOrganizer($rawArray, 3);
-		return $coeff * $sum;
+		$normalizer = 1 / (2 * (count($rawData) - 1) * pow($mu, 2));
+		for ($i = 0; $i < $this->allanTime($rawData); ++$i) {
+			$organizedArray = $this->dataOrganizer($rawData, $i + 1);
+			$averageOrgArray = $this->average2($organizedArray);
+			$sum = $this->summation($averageOrgArray);
+		}
+		return $normalizer * $sum;
+		
 		// return $sum;
+		
 	}// end of allan variace 
 	
 	// Supporting Math Functions 
@@ -60,21 +68,49 @@ class allanCalc {
 	 * Can also be thought of as the number of elements in a cluster to averag
 	 * @return Ambigous <multitype:, unknown> : 
 	 */
-	private function dataOrganizer($rawArray, $elements) {
+	private function dataOrganizer($oneDArray, $elements) {
 		$tree = array();
-		for ($i = 0; $i < count($rawArray); ++$i) {
+		for ($i = 0; $i < count($oneDArray); ++$i) {
 			$value = 0;
 			for ($j = 0; $j < $elements; ++$j) {
-				$tree[$i][$j] = $rawArray[$i];
-			}
-		}
-		foreach ($tree as $branch) {
-			foreach ($branch as $leaf) {
-				echo $leaf . "<br>";
+				$tree[$i][$j] = $oneDArray[$i];
 			}
 		}
 		return $tree;
 	}
+	/**
+	 * 
+	 * @param unknown $array
+	 * @return multitype:number
+	 * The purpose here is to be able to able to handle averaging every two nu,bers every three and so forth.
+	 * the plan is to have a multidimentional array whose sub arrays are the length of two, three, or how ever many numbers we want.
+	 * This is to facilitate another functions ability to average the smaller arrays  later. 
+	 * 
+	 */
+	private function average2($array) {
+		$newArray = array();
+		for ($i = 0; $i < count($array); ++$i) {
+			$value = 0;
+			for ($j = 0; $j < count($array[$i]); ++$j) {
+				$value += $array[$i][$j];
+			}
+			$newArray[$i] = $value / count($array[$i]);
+		}
+		return $newArray;
+	}
+	
+	/**
+	 * 
+	 * @param unknown $data: original data array 
+	 * @return number: This is the number of increments that must happeb to go from 50 ms to 300 ms. 
+	 * If we are to plot so many points in 300s, then 300 over then number of points 
+	 * will give the increment from 50 to 300. 
+	 */
+	private function allanTime($data) {
+		return 300 / count($data);
+	}
+	 
+
 } // end of allan calculator class 
 
 
