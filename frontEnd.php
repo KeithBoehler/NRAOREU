5f6ab4bceeac2a5ff0bb81e4adcbc22666cdfc9f
Keith Boehler <germanbohler@gmail.com>
2015-07-29 17:50:26 -0400

    Made basic form to get user input.

---
+++ frontEnd.php
@@ -0,0 +1,10 @@
+<?php
+
+
+?>
+
+<form action="frontEnd.php" method="POST" enctype="multipart/form-data">
+	<input type="file" name="file"><br><br>
+	<input type="submit" name="Submit">
+</form>
+	
