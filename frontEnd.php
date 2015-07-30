c00f286a5c9b8e321780993bfb01bd992ec9168f
Keith Boehler <germanbohler@gmail.com>
2015-07-30 13:56:24 -0400

    This is a trial front end to simulate how the program will handel user given information.

--- frontEnd.php
+++ frontEnd.php
@@ -1,10 +1,41 @@
 <?php
+require 'fileWorker.php';
 
+class frontEnd {
+	
+	private $name;
+	private $tempName;
+	
+	public function __construct() {
+		$this->$name = $_FILES['files']['names'];
+		$this->tempName = $_FILES['files']['tempName'];
+		$this->didSubmit();
+	}
+	
+	
+	/**
+	 * This is simply to verfy that a file was submited. 
+	 */
+	private function didSubmit() {
+		if (isset($this->name)) {
+			if (!empty($this->name)) {
+				echo "File uploaded.";
+			}
+			else {
+				echo "Please choose a file. ";
+			}
+		}
+	}
+	
+	
+}
+
+$f = new frontEnd();
 
 ?>
 
 <form action="frontEnd.php" method="POST" enctype="multipart/form-data">
-	<input type="file" name="file"><br><br>
-	<input type="submit" name="Submit">
+	<input type="files" name="files"><br><br> 
+	<input type="submit" value="Submit"> 
 </form>
 	
