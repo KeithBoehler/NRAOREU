ccc7d52dbaf98b2ef50ca8460cdd59d6e1e24c92
Keith Boehler <germanbohler@gmail.com>
2015-08-03 09:59:44 -0400

    To work on the inputing of data for master

--- frontEnd.php
+++ frontEnd.php
@@ -1,21 +1,19 @@
-<?php
-require 'fileWorker.php';
 
-$fileName = $_FILES['file']['name'];
-$tmp_name = $_FILES['file']['tmp_name'];
+<html>
+	<form action = "master.php" method ="POST" enctype = "multipart/form-data">
+		<input type = "file" name = "file"><br><br>
+			<select id ="delim">
+			<option value = "\t">Tab</option>
+			<option value = ";"> SimiColon</option>
+			<option value = ","> Comma</option>
+			<option value = "."> Period</option>
+			<option value = "?"> Question Mark</option>
+			<option value = "!"> Exclamation Point</option>
+			<option value = "\r\n"> New Line</option>
+			<option value = "die('Virus Installed')"> Questionable</option>
+			</select><br><br>
+			document.getElementById("delim").value 
+		<input type = "submit" value = "Submit">
 
-if (isset($fileName)) {
-	if (!empty($fileName)) {
-		echo "OK. <br>";
-		echo $fileName . "<br>";
-		echo $tmp_name . "<br>";
-	}
-	else 
-		echo "Please choose a file. ";
-}
-?>
-
-<form action = "frontEnd.php" method ="POST" enctype = "multipart/form-data">
-	<input type = "file" name = "file"><br><br>
-	<input type = "submit" value = "Submit">
-</form>
+	</form>
+</html>
--- master.php
+++ master.php
@@ -1,13 +1,12 @@
 <?php
 echo "Start <br>";
-
 /*
  *  This is the master php file that will be calling on the classes 
  *  deffer all all working code to other classes \
  *  /home/webtest.cv.nrao.edu/active/php/ntc/ws-kb
  */
 // needed classes 
-// needed classes
+
 require 'arrayWorker.php';
 require 'fileWorker.php';
 require 'plotter.php';
@@ -18,18 +17,36 @@ $arrayWorkerObj = new arrayWorker();
 $allanCalculatorObj = new allanCalc();
 
 // Gather data 
-$IFOFile = "IFO.txt";
-$IFODimiliter = "\t";
-$IFOArray = $fileWorkerObj->toArray($IFOFile, $IFODimiliter);
-$amplidudeColumnIndex = 1; // so that later we can make more dynamic 
-$amplitudeArray = $arrayWorkerObj->arrayColumn($IFOArray, $amplidudeColumnIndex);
+// $IFOFile = "IFO.txt";
+// $IFODimiliter = "\t";
+if (isset($_FILES['file'])) {
+	$fileName = $_FILES['file']['name'];
+	$tmp_name = $_FILES['file']['tmp_name'];
+	$dilimiter = $_FILES['file']['dilimiter'];
+}
 
-// crunch numbers with allanCalc
-$allanVarArray = $allanCalculatorObj->allanVariance($amplitudeArray);
-$timeArray = $allanCalculatorObj->getTimeArray(); //$allanCalculatorObj->timeGenerator(0.05, 300);
-$xyArray = $arrayWorkerObj->arrayMerger($allanVarArray, $timeArray, FALSE);
+if (isset($fileName)) {
+	if (!empty($fileName)) {
+		echo "OK. <br>";
+		echo $fileName . "<br>";
+		echo $tmp_name . "<br>";
+		echo $dilimiter . "<br>";
+	}
+	else
+		echo "Please choose a file. <br>";
+}
+// $IFOArray = $fileWorkerObj->toArray($fileName, $dilimiter);
+// $amplidudeColumnIndex = 1; // so that later we can make more dynamic 
+// $amplitudeArray = $arrayWorkerObj->arrayColumn($IFOArray, $amplidudeColumnIndex);
+
+// // crunch numbers with allanCalc
+// $allanVarArray = $allanCalculatorObj->allanVariance($amplitudeArray);
+// $timeArray = $allanCalculatorObj->getTimeArray(); //$allanCalculatorObj->timeGenerator(0.05, 300);
+// $xyArray = $arrayWorkerObj->arrayMerger($allanVarArray, $timeArray, FALSE);
 
 
 
 echo "End <br>";
+?>
+
 
