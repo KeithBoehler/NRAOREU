abae14fe0b8e0b897ad7bc75ef771246f170b184
Keith Boehler <germanbohler@gmail.com>
2015-08-03 13:13:33 -0400

    Doing awat with the frontEnd.php file its best to jsut have it as part of master.php. Also fixed error with getting values form the html drop down.

--- master.php
+++ master.php
@@ -22,27 +22,25 @@ $allanCalculatorObj = new allanCalc();
 if (isset($_FILES['file'])) {
 	$fileName = $_FILES['file']['name'];
 	$tmp_name = $_FILES['file']['tmp_name'];
-	$dilimiter = $_FILES['file']['dilimiter'];
+	$delimiter = $_REQUEST['delimiter'];
 }
 
 if (isset($fileName)) {
 	if (!empty($fileName)) {
-		echo "OK. <br>";
-		echo $fileName . "<br>";
-		echo $tmp_name . "<br>";
-		echo $dilimiter . "<br>";
+		$IFOArray = $fileWorkerObj->toArray($fileName, $delimiter);
+		$amplidudeColumnIndex = 1; // so that later we can make more dynamic
+		$amplitudeArray = $arrayWorkerObj->arrayColumn($IFOArray, $amplidudeColumnIndex);
+		
+		// crunch numbers with allanCalc
+		$allanVarArray = $allanCalculatorObj->allanVariance($amplitudeArray);
+		$timeArray = $allanCalculatorObj->getTimeArray(); //$allanCalculatorObj->timeGenerator(0.05, 300);
+		$xyArray = $arrayWorkerObj->arrayMerger($allanVarArray, $timeArray, FALSE);
 	}
 	else
 		echo "Please choose a file. <br>";
 }
-// $IFOArray = $fileWorkerObj->toArray($fileName, $dilimiter);
-// $amplidudeColumnIndex = 1; // so that later we can make more dynamic 
-// $amplitudeArray = $arrayWorkerObj->arrayColumn($IFOArray, $amplidudeColumnIndex);
 
-// // crunch numbers with allanCalc
-// $allanVarArray = $allanCalculatorObj->allanVariance($amplitudeArray);
-// $timeArray = $allanCalculatorObj->getTimeArray(); //$allanCalculatorObj->timeGenerator(0.05, 300);
-// $xyArray = $arrayWorkerObj->arrayMerger($allanVarArray, $timeArray, FALSE);
+//document.getElementById("delim").value 
 
 
 
@@ -50,3 +48,21 @@ echo "End <br>";
 ?>
 
 
+<html>
+	<form action = "master.php" method ="POST" enctype = "multipart/form-data">
+		<input type = "file" name = "file"><br><br>
+		<select name = "delimiter">
+			<option value = "\t">Tab</option>
+			<option value = ";"> Semi Colon</option>
+			<option value = ","> Comma</option>
+			<option value = "."> Period</option>
+			<option value = "?"> Question Mark</option>
+			<option value = "!"> Exclamation Point</option>
+			<option value = "\r\n"> New Line</option>			
+			<option value = "die('Virus Installed')"> Questionable</option>
+			</select><br><br>
+			
+		<input type = "submit" value = "Submit">
+
+	</form>
+</html>
